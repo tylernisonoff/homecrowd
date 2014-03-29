108d147920d5e1ccee7a754c26cc683052c913df
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-03-29 15:27:31 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homecrowd::Application.config.secret_token = '5e9fb10fa165fce7b14541c5a92d2423c92ca841989704a57d7dfa0a111e560dd2b57bd95ea88f27e8f0510e66dcf5fe9db0b73f41dd438c009eb8e3f9c611ab'
