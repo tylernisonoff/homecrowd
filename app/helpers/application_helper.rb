cb2c84131cb27f0b0757c35f560d975505de3f24
Michael Lai <themichaellai@gmail.com>
2014-03-30 06:34:33 -0400

    Profile page styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     return false if !user_signed_in?
     "Student".eql?(current_user.type)
   end
+
+  def avatar_url(user, width: 60)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://robohash.org/#{gravatar_id}.png"
+  end
 end
