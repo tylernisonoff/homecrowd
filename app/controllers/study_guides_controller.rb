b6b9eba0a6ba8471ff9955fd1cd6ff7be0947832
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-03-30 06:04:29 -0400

    added urls for study guides

--- app/controllers/study_guides_controller.rb
+++ app/controllers/study_guides_controller.rb
@@ -1,3 +1,6 @@
+require "net/http"
+require "uri"
+
 class StudyGuidesController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
 
@@ -10,6 +13,7 @@ class StudyGuidesController < ApplicationController
     @classroom = Classroom.find params[:classroom_id]
     @study_guide = @classroom.study_guides.build params[:study_guide]
     @study_guide.user = current_user
+    @study_guide.doc_url = get_url
     if @study_guide.save
       save_action(:create, @study_guide)
       redirect_to classroom_study_guide_path(@classroom, @study_guide)
@@ -21,5 +25,11 @@ class StudyGuidesController < ApplicationController
   def show
     @study_guide = StudyGuide.find params[:id]
     save_action(:view, @study_guide)
+  end
+  
+  private
+  def get_url
+    endpoint = "http://boiling-garden-7787.herokuapp.com/"
+    Net::HTTP.get_response(URI.parse(endpoint)).body
   end
 end
