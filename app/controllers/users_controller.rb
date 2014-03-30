698dd35def65404a435510afc4ba2f36b5fb8404
Michael Lai <themichaellai@gmail.com>
2014-03-30 07:18:31 -0400

    limit actions fetched

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @actions = UserAction.where(user_id: @user).order('created_at DESC')
+    @actions = UserAction.where(user_id: @user).order('created_at DESC').limit(10)
   end
 end
